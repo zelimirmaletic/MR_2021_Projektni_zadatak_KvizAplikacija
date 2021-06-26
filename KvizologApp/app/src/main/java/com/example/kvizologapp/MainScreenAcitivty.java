bf65795749d38d3fed766ae7278827ebc54f95ce
zelimirmaletic <mzeljo417@gmail.com>
2021-06-26 19:57:45 +0200

    Zavrsena navigacija u glavnoj aktivnosti aplikacije.

--- KvizologApp/app/src/main/java/com/example/kvizologapp/MainScreenAcitivty.java
+++ KvizologApp/app/src/main/java/com/example/kvizologapp/MainScreenAcitivty.java
@@ -1,30 +1,55 @@
 package com.example.kvizologapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainScreenAcitivty extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    Button btnLogout;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen_acitivty);
         mAuth = FirebaseAuth.getInstance();
-        btnLogout = findViewById(R.id.btnLogout);
-        btnLogout.setOnClickListener(v -> {
-            signOut();
-        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener  navListener = item -> {
+        Fragment selectedFragment = null;
+        switch (item.getItemId()){
+            case R.id.homeFragment:
+                selectedFragment = new HomeFragment();
+                break;
+            case R.id.settingsFragment:
+                selectedFragment = new SettingsFragment();
+                break;
+            case R.id.statisticsFragment:
+                selectedFragment = new StatisticsFragment();
+                break;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,selectedFragment).commit();
+        return true;
+    };
+
     private void signOut(){
         mAuth.signOut();
         startActivity(new Intent(MainScreenAcitivty.this, MainActivity.class));
