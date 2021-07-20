1d4dee3f0baac54ea87e018495e137a8acd21ad6
zelimirmaletic <mzeljo417@gmail.com>
2021-07-21 00:13:14 +0200

    Implemented sharing results on Facebook.

--- KvizologApp/app/src/main/java/com/example/kvizologapp/QuizResultsFragment.java
+++ KvizologApp/app/src/main/java/com/example/kvizologapp/QuizResultsFragment.java
@@ -1,53 +1,60 @@
 package com.example.kvizologapp;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.facebook.CallbackManager;
+import com.facebook.share.model.ShareHashtag;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
+
 public class QuizResultsFragment extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    private String mParam1;
-    private String mParam2;
-
     TextView txvPoints;
-
-
+    ImageButton btnFacebook;
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
 
     public QuizResultsFragment() {
     }
 
-    public static QuizResultsFragment newInstance(String param1, String param2) {
-        QuizResultsFragment fragment = new QuizResultsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_quiz_results, container, false);
-        txvPoints = (TextView)view.findViewById(R.id.txbResultPoints);
-
+        txvPoints = view.findViewById(R.id.txbResultPoints);
+        btnFacebook = view.findViewById(R.id.btnFacebook);
         //Set points number
         txvPoints.setText((++QuizGameActivity.POINTS_COUNTER) + "/20");
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(getResources().getString(R.string.results_message));
+        stringBuilder.append(txvPoints.getText().toString());
+        stringBuilder.append(getResources().getString(R.string.chek_out_message));
+        btnFacebook.setOnClickListener(v -> {
+            ShareLinkContent.Builder contentBulder = new ShareLinkContent.Builder()
+                    //URL To Google Playstore App Page TO-DO
+                    .setContentUrl(Uri.parse("https://developers.facebook.com"))
+                    //Set number of points
+                    .setQuote(stringBuilder.toString())
+                    //Set hashtags for both languages
+                    .setShareHashtag(new ShareHashtag.Builder().setHashtag(getResources().getString(R.string.hashtag_quizolog)).build());
+            ShareDialog shareDialog = new ShareDialog(QuizResultsFragment.this);
+            shareDialog.show(contentBulder.build(), ShareDialog.Mode.AUTOMATIC);
+        });
         return view;
     }
 
